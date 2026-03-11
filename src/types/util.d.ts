bdfad3380093da4d9615b8bcf7e7c0465ccba97d
tetarchus <8436118+tetarchus@users.noreply.github.com>
2026-03-11 09:30:06 +0000

    feat: add `Awaitable` type

--- src/types/util.d.ts
+++ src/types/util.d.ts
@@ -1,7 +1,10 @@
 /** A type that may or may not be an array. */
 type Arrayable<T> = T | T[];
 
+/** A type that may or may not need awaiting. */
+type Awaitable<T> = T | Promise<T>;
+
 /** A type that may or may not be null/undefined. */
 type Nullable<T> = T | null | undefined;
 
-export type { Arrayable, Nullable };
+export type { Arrayable, Awaitable, Nullable };
